b6e6c84fb92b822636abbcf9219d5c7b22485b18
ylaaan <drangony@gmail.com>
2025-11-05 19:08:22 +0100

    Add bash completion

--- init.lua
+++ init.lua
@@ -735,9 +735,9 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
-
+        bashls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
